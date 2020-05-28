ee5f1bd1c4349419b6ab894b74999213a9a5851d
Vinicius Moura <vmoura56@gmail.com>
2020-05-28 00:47:11 -0300

    Add ingredient db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ require 'json'
 require 'open-uri'
 
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-cocktails_serialized = open(url).read
-cocktails = JSON.parse(cocktails_serialized)
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
 
-cocktails.each do |cocktail|
-  Cocktail.create(name: cocktail)
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
 end
